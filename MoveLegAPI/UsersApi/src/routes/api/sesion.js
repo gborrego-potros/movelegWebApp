9d171b0770f45cf524306bcfe1a44cc8ffc4b082
SopitasWick <jesus.izaguirre216660@potros.itson.edu.mx>
2025-07-09 12:37:31 -0700

    C.U. Finalizar terapia

--- MoveLegAPI/UsersApi/src/routes/api/sesion.js
+++ MoveLegAPI/UsersApi/src/routes/api/sesion.js
@@ -7,9 +7,37 @@ router.get('/', async (req, res)=>{
     res.json(resultado);
 });
 
+//Se utilizara para guardar la sesión al finalizar la terapia
 router.post('/', async(req, res) => {
-    const resultado = await Sesion.create(req.body);
-    res.json(resultado);
+    //CUERPO DE LA PETICION DEL JUEGO SERIO
+    const { 
+        promedioVelocidadRodilla, 
+        promedioVelocidadTobillo, 
+        posicionRodillaMin,
+        posicionRodillaMax,
+        anguloTobilloMin,
+        anguloTobilloMax,
+        numRepeticionesTobillo, 
+        numRepeticionesRodilla } = req.body;
+try{
+// Crear nuevo resultado de terapia asociado al usuario
+    const nuevoRegistro = await Sesion.create({
+      promedioVelocidadRodilla,
+      promedioVelocidadTobillo,
+      posicionRodillaMin,
+      posicionRodillaMax,
+      anguloTobilloMin,
+      anguloTobilloMax,
+      numRepeticionesTobillo,
+      numRepeticionesRodilla
+    });
+
+res.status(201).json(nuevoRegistro);
+}catch(error){
+console.error(error);
+res.status(500).json({ error: 'Error al guardar resultado de terapia' });  
+}
+
 });
 
 router.put('/:calibracionId', async(req, res) => {
